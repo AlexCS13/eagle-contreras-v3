80b038da59a13fabf7ea7266907e5b88b6d8ae12
AlexCS13 <alexandercs_@hotmail.com>
2022-06-29 15:21:47 -0500

    Creacion de website en react

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eagle Simulation & Training WEBSITE V3 - WORK IN PROGRESS
         </p>
+        <p>🦅🦅🦅</p>
         <a
           className="App-link"
           href="https://reactjs.org"
