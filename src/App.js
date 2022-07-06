d3bb4e4eb426c9c39ffec729f5e1c7923ea632a9
AlexCS13 <alexandercs_@hotmail.com>
2022-07-05 20:12:40 -0500

    navbar en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eagle Simulation & Training WEBSITE V3 - WORK IN PROGRESS
-        </p>
-        <p>🦅🦅🦅</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container-fluid" data-aos="fade-down">
+          <div className="row ">
+            <div className="col-12 col-md-12 col-lg-12">
+              <nav className="navbar navbar-expand-lg navbar-expand-md navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">
+                  {/* <img src="./assets/img/logo_eagle_claro.png" className="logo" alt="eagle"> */}
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav">
+                    {/* <li className="nav-item active">
+                      <a className="nav-link" href="#">Inicio<a>
+                    </li> */}
+                    <li className="nav-item">
+                      <a className="nav-link" href="./pages/productos.html">Productos</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./pages/experiencias.html">Experiencias</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./pages/quienes_somos.html">Quienes somos</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./pages/contacto.html">Contacto</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
